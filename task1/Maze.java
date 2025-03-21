d293dc18348884354d03bd71c2e5749dab5bb80d
Norfend <Fenker@protonmail.com>
2025-03-21 20:10:38 +0100

    Implemented new version of the task with name "Maze"

--- task1/Maze.java
+++ task1/Maze.java
@@ -7,99 +7,98 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.System.exit;
-import static java.lang.System.in;
 
 /**
  * Standard Depth-first search algorithm with preliminary input data checks
- *
- * @return Outputs errors to the System.err or a maze with the required points into system.out
  */
 
 public class Maze {
     //Static variable for the maze
     private static final ArrayList<char[]> labyrinth = new ArrayList<>();
 
-    public void maze() {
+    public static void main(String[] args) {
+        int counter = 0;
+        Pattern allowedCharacters = Pattern.compile("^[.#]*$");
+        Pattern allowedUpperBorder = Pattern.compile("^#\\.#+$");
+        Pattern allowedBorder = Pattern.compile("^#.*#$");
+        Pattern allowedLowerBorder = Pattern.compile("^#+\\.#$");
+
+        Matcher allowedCharactersMatcher;
+        Matcher upperBorderMatcher;
+        Matcher lowerBorderMatcher;
+        Matcher borderMatcher;
+
         Scanner scanner = new Scanner(System.in);
-        //Labyrinth width
+
+        //Check labyrinth width
         int width = -1;
+
         //Labyrinth loading
         while (scanner.hasNext()) {
-            //Check labyrinth width
-            if (labyrinth.size() > 50) {
-                System.err.println("Error: Delka bludiste je mimo rozsah!");
-                exit(1);
-            }
-            String temporary = scanner.next();
-            //Check labyrinth length
+            String temporary = scanner.nextLine();
+            allowedCharactersMatcher = allowedCharacters.matcher(temporary);
+
             if (width == -1) {
                 width = temporary.length();
-                if (width < 5 || 100 < width) {
-                    System.err.println("Error: Sirka bludiste je mimo rozsah!");
-                    exit(1);
-                }
-            } else if (width != temporary.length()) {
-                System.err.println("Error: Bludiste neni obdelnikove!");
-                exit(1);
             }
+
+            //Check labyrinth symbols
+            if (!allowedCharactersMatcher.find()) {
+                errorChecker.setWrongSymbols();
+            }
+            
+            //Check that labyrinth is rectangle
+            if (temporary.length() != width) {
+                errorChecker.setNotRectangle();
+            }
+
+            //Check labyrinth borders
+            if (counter == 0) {
+                upperBorderMatcher = allowedUpperBorder.matcher(temporary);
+                if (! upperBorderMatcher.find()) {
+                    errorChecker.setWrongBorders();
+                }
+            } else {
+                borderMatcher = allowedBorder.matcher(temporary);
+                if (! borderMatcher.find()) {
+                    errorChecker.setWrongBorders();
+                }
+            }
+
             labyrinth.add(temporary.toCharArray());
+            counter++;
         }
+
+        //Check labyrinth width
+        if (width < 5 || 100 < width) {
+            errorChecker.setWrongWidth();
+        }
+
+        lowerBorderMatcher = allowedLowerBorder.matcher(new String(labyrinth.getLast()));
+        if (! lowerBorderMatcher.find()) {
+            errorChecker.setWrongBorders();
+        }
+
         //Variables for entrance and exit of the maze
         Point entrance = new Point(1, 0);
         Point exit = new Point(width - 2, labyrinth.size() - 1);
 
-        //Check labyrinth width
-        if (labyrinth.size() < 5) {
-            System.err.println("Error: Delka bludiste je mimo rozsah!");
-            exit(1);
-        }
-        //Check labyrinth entrance
-        else if (labyrinth.get(entrance.y)[entrance.x] != '.') {
-            System.err.println("Error: Vstup neni vlevo nahore!");
-            exit(1);
-        }
-        //Check labyrinth exit
-        else if (labyrinth.get(exit.y)[exit.x] != '.') {
-            System.err.println("Error: Vystup neni vpravo dole!");
-            exit(1);
+        //Check labyrinth length
+        if (labyrinth.size() < 5 || 50 < labyrinth.size()) {
+            errorChecker.setWrongLength();
         }
 
-        //Check labyrinth symbols
-        for (char[] chars : labyrinth) {
-            Pattern pattern = Pattern.compile("^[.#]*$");
-            Matcher matcher = pattern.matcher(new String(chars));
-            if (!matcher.find()) {
-                System.err.println("Error: Bludiste obsahuje nezname znaky!");
-                exit(1);
-            }
+        //Check labyrinth entrance
+        if (labyrinth.get(entrance.y)[entrance.x] != '.') {
+            errorChecker.setWrongEntrance();
         }
-        //Check labyrinth symbols (to complete task requirements)
-        for (int i = 0; i < labyrinth.size(); i++) {
-            if (i == 0) {
-                Pattern patternBorder = Pattern.compile("^[#]\\.[#]{" + (labyrinth.get(i).length - 2) + "}$");
-                Matcher matcher = patternBorder.matcher(new String(labyrinth.get(i)));
-                if (!matcher.find()) {
-                    System.err.println("Error: Bludiste neni oplocene!");
-                    exit(1);
-                }
-            }
-            else if (i == labyrinth.size() - 1) {
-                Pattern patternBorder = Pattern.compile("^[#]{" + (labyrinth.get(i).length - 2) +"}\\.[#]$");
-                Matcher matcher = patternBorder.matcher(new String(labyrinth.get(i)));
-                if (!matcher.find()) {
-                    System.err.println("Error: Bludiste neni oplocene!");
-                    exit(1);
-                }
-            }
-            else {
-                Pattern patternBorder = Pattern.compile("^[#].*[#]$");
-                Matcher matcher = patternBorder.matcher(new String(labyrinth.get(i)));
-                if (!matcher.find()) {
-                    System.err.println("Error: Bludiste neni oplocene!");
-                    exit(1);
-                }
-            }
+
+        //Check labyrinth exit
+        if (labyrinth.get(exit.y)[exit.x] != '.') {
+            errorChecker.setWrongExit();
         }
+
+        errorChecker.printChecks();
 
         //Check path
         boolean[][] isVisited = new boolean[width][labyrinth.size()];
@@ -108,10 +107,12 @@ public class Maze {
             recursiveDepthFirstSearch(entrance, exit, isVisited, path);
         }
         //Saving time by forcibly exiting recursion
-        catch (Exception e) {
+        catch (Exception ignored) {
+
         }
+
         //Checking the existence of a path
-        if (path.size() == 0) {
+        if (path.isEmpty()) {
             System.err.println("Error: Cesta neexistuje!");
             exit(1);
         }
@@ -124,22 +125,21 @@ public class Maze {
                 recursiveDepthFirstSearch(entrance, exit, visited, new ArrayList<>());
                 labyrinth.get(point.y)[point.x] = '!';
             }
+            //Saving time by forcibly exiting recursion
             catch (Exception e) {
                 labyrinth.get(point.y)[point.x] = '.';
             }
         }
         //Final polishing and printing of labyrinth
         labyrinth.get(entrance.y)[entrance.x] = '!';
-        printArray(labyrinth);
+        printArray();
     }
 
     /**
      * Printing of the labyrinth into system.out
-     *
-     * @param inputArray Printable array variable
      */
-    private static void printArray(ArrayList<char[]> inputArray) {
-        for (char[] row: inputArray) {
+    private static void printArray() {
+        for (char[] row: Maze.labyrinth) {
             System.out.println(row);
         }
     }
@@ -155,7 +155,7 @@ public class Maze {
      *
      * @param inputPath Array for visited "vertices"
      */
-    private void recursiveDepthFirstSearch(Point current, Point end, boolean[][] inputVisited,
+    private static void recursiveDepthFirstSearch(Point current, Point end, boolean[][] inputVisited,
                                                   ArrayList<Point> inputPath) {
         inputVisited[current.x][current.y] = true;
         if (current.x == end.x && current.y == end.y) {
@@ -167,7 +167,7 @@ public class Maze {
             for (Point route : routes) {
                 inputPath.add(current);
                 recursiveDepthFirstSearch(route, end, inputVisited, inputPath);
-                inputPath.remove(inputPath.get(inputPath.size() - 1));
+                inputPath.remove(inputPath.getLast());
             }
         }
     }
@@ -181,10 +181,10 @@ public class Maze {
      *
      * @return ArrayList of possible shifts from an input point
      */
-    private ArrayList<Point> checkRoutes(Point inputPoint, boolean[][] inputVisited) {
+    private static ArrayList<Point> checkRoutes(Point inputPoint, boolean[][] inputVisited) {
         ArrayList<Point> result = new ArrayList<>();
         //Check right
-        if ((inputPoint.x + 1) < (labyrinth.get(labyrinth.size() - 1).length - 1) &&
+        if ((inputPoint.x + 1) < (labyrinth.getLast().length - 1) &&
                 labyrinth.get(inputPoint.y)[inputPoint.x + 1] != '#' && !inputVisited[inputPoint.x + 1][inputPoint.y])
             result.add(new Point((inputPoint.x + 1), inputPoint.y));
         //Check down
@@ -200,5 +200,82 @@ public class Maze {
                 !inputVisited[inputPoint.x][inputPoint.y - 1])
             result.add(new Point(inputPoint.x, (inputPoint.y - 1)));
         return result;
+    }
+
+    private static class errorChecker {
+        private static Boolean notRectangle = false;
+        private static Boolean wrongEntrance = false;
+        private static Boolean wrongExit = false;
+        private static Boolean wrongWidth = false;
+        private static Boolean wrongLength = false;
+        private static Boolean wrongSymbols = false;
+        private static Boolean wrongBorders = false;
+
+        public static void printChecks() {
+            if (notRectangle) {
+                System.err.println("Error: Bludiste neni obdelnikove!");
+                exit(1);
+            } else if (wrongEntrance) {
+                System.err.println("Error: Vstup neni vlevo nahore!");
+                exit(1);
+            } else if (wrongExit) {
+                System.err.println("Error: Vystup neni vpravo dole!");
+                exit(1);
+            } else if (wrongWidth) {
+                System.err.println("Error: Sirka bludiste je mimo rozsah!");
+                exit(1);
+            } else if (wrongLength) {
+                System.err.println("Error: Delka bludiste je mimo rozsah!");
+                exit(1);
+            } else if (wrongSymbols) {
+                System.err.println("Error: Bludiste obsahuje nezname znaky!");
+                exit(1);
+            } else if (wrongBorders) {
+                System.err.println("Error: Bludiste neni oplocene!");
+                exit(1);
+            }
+        }
+
+        public static void setNotRectangle() {
+            if (! notRectangle) {
+                notRectangle = true;
+            }
+        }
+
+        public static void setWrongEntrance() {
+            if (!wrongEntrance) {
+                errorChecker.wrongEntrance = true;
+            }
+        }
+
+        public static void setWrongExit() {
+            if (!wrongExit) {
+                wrongExit = true;
+            }
+        }
+
+        public static void setWrongWidth() {
+            if (!wrongWidth) {
+                wrongWidth = true;
+            }
+        }
+
+        public static void setWrongLength() {
+            if (!wrongLength) {
+                wrongLength = true;
+            }
+        }
+
+        public static void setWrongSymbols() {
+            if (! wrongSymbols) {
+                wrongSymbols = true;
+            }
+        }
+
+        public static void setWrongBorders() {
+            if (! wrongBorders) {
+                wrongBorders = true;
+            }
+        }
     }
 }
